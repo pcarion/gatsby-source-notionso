06d82c74feb5dda310dc69bab3cbad0993c2265e
Pierre Carion <pcarion@gmail.com>
2019-12-26 15:45:51 -0800

    add extra test with long string

--- test/parseNotionText.test.ts
+++ test/parseNotionText.test.ts
@@ -67,4 +67,176 @@ describe('parseNotionText', () => {
       ],
     ]);
   });
+  it('should parse a complex string', () => {
+    const result = parseNotionText([
+      ['This is '],
+      ['bold', [['b']]],
+      [' and '],
+      ['italic ', [['i']]],
+      ['and '],
+      ['stroke', [['s']]],
+      [' and '],
+      ['code', [['c']]],
+      [' and a '],
+      ['link', [['a', 'http://www.google.com']]],
+      [' and a '],
+      ['red text', [['h', 'red']]],
+      ['. This is '],
+      ['bold and italic', [['b'], ['i']]],
+      ['.'],
+    ]);
+    expect(result).toEqual([
+      [
+        'This is ',
+        {
+          isBold: false,
+          isItalic: false,
+          isStrikeThrough: false,
+          isCode: false,
+          isLink: false,
+        },
+      ],
+      [
+        'bold',
+        {
+          isBold: true,
+          isItalic: false,
+          isStrikeThrough: false,
+          isCode: false,
+          isLink: false,
+        },
+      ],
+      [
+        ' and ',
+        {
+          isBold: false,
+          isItalic: false,
+          isStrikeThrough: false,
+          isCode: false,
+          isLink: false,
+        },
+      ],
+      [
+        'italic ',
+        {
+          isBold: false,
+          isItalic: true,
+          isStrikeThrough: false,
+          isCode: false,
+          isLink: false,
+        },
+      ],
+      [
+        'and ',
+        {
+          isBold: false,
+          isItalic: false,
+          isStrikeThrough: false,
+          isCode: false,
+          isLink: false,
+        },
+      ],
+      [
+        'stroke',
+        {
+          isBold: false,
+          isItalic: false,
+          isStrikeThrough: true,
+          isCode: false,
+          isLink: false,
+        },
+      ],
+      [
+        ' and ',
+        {
+          isBold: false,
+          isItalic: false,
+          isStrikeThrough: false,
+          isCode: false,
+          isLink: false,
+        },
+      ],
+      [
+        'code',
+        {
+          isBold: false,
+          isItalic: false,
+          isStrikeThrough: false,
+          isCode: true,
+          isLink: false,
+        },
+      ],
+      [
+        ' and a ',
+        {
+          isBold: false,
+          isItalic: false,
+          isStrikeThrough: false,
+          isCode: false,
+          isLink: false,
+        },
+      ],
+      [
+        'link',
+        {
+          isBold: false,
+          isItalic: false,
+          isStrikeThrough: false,
+          isCode: false,
+          isLink: true,
+          withLink: 'http://www.google.com',
+        },
+      ],
+      [
+        ' and a ',
+        {
+          isBold: false,
+          isItalic: false,
+          isStrikeThrough: false,
+          isCode: false,
+          isLink: false,
+        },
+      ],
+      [
+        'red text',
+        {
+          isBold: false,
+          isItalic: false,
+          isStrikeThrough: false,
+          isCode: false,
+          isLink: false,
+        },
+      ],
+      [
+        '. This is ',
+        {
+          isBold: false,
+          isItalic: false,
+          isStrikeThrough: false,
+          isCode: false,
+          isLink: false,
+        },
+      ],
+      [
+        'bold and italic',
+        {
+          isBold: true,
+          isItalic: true,
+          isStrikeThrough: false,
+          isCode: false,
+          isLink: false,
+        },
+      ],
+      [
+        '.',
+        {
+          isBold: false,
+          isItalic: false,
+          isStrikeThrough: false,
+          isCode: false,
+          isLink: false,
+        },
+      ],
+    ]);
+  });
 });
