62198b6c216549bc13e961c968d4a8a3f0bfb40a
Pierre Carion <pcarion@gmail.com>
2020-01-04 17:40:29 -0800

    add tests

--- test/renderNotionBlocks.test.ts
+++ test/renderNotionBlocks.test.ts
@@ -2,6 +2,8 @@ import renderNotionBlocks from '../src/renderer/renderNotionBlocks';
 import { NotionPageBlock } from '../src/types/notion';
 import renderFuncsForTests from './renderFuncsForTests';
 
+// for xml beaufifier: https://codebeautify.org/xmlviewer
+
 type Fixtures = { pageId: string; in: NotionPageBlock[]; out: string }[];
 
 const fixtures: Fixtures = [
@@ -68,7 +70,276 @@ const fixtures: Fixtures = [
         ],
       },
     ],
-    out: '<text.x><text>para1</text><text>para2</text></text.x>',
+    out: `
+      <text.x>
+        <text>para1</text>
+        <text>para2</text>
+      </text.x>
+    `,
+  },
+  {
+    pageId: '517b209c-981e-439e-9b8f-bd04d4ca48a8',
+    in: [
+      {
+        blockId: '517b209c-981e-439e-9b8f-bd04d4ca48a8',
+        blockIds: [
+          '3c0878e1-e6c5-4e85-b757-8355262577bf',
+          '157cbd6b-a2ac-4dd1-b2d5-9c3e5eac6683',
+        ],
+        type: 'page',
+        attributes: [
+          {
+            att: 'pageIcon',
+            value: '',
+          },
+        ],
+        properties: [
+          {
+            propName: 'title',
+            value: [
+              {
+                text: 'page 2',
+                atts: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        blockId: '3c0878e1-e6c5-4e85-b757-8355262577bf',
+        blockIds: [
+          '6664db04-32e0-4996-9a30-2a5e353c5902',
+          '1f21514e-b820-4ac5-972a-dfdcfc4403aa',
+        ],
+        type: 'bulleted_list',
+        attributes: [],
+        properties: [
+          {
+            propName: 'title',
+            value: [
+              {
+                text: 'list1',
+                atts: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        blockId: '6664db04-32e0-4996-9a30-2a5e353c5902',
+        blockIds: [],
+        type: 'bulleted_list',
+        attributes: [],
+        properties: [
+          {
+            propName: 'title',
+            value: [
+              {
+                text: 'list1.1',
+                atts: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        blockId: '1f21514e-b820-4ac5-972a-dfdcfc4403aa',
+        blockIds: [],
+        type: 'bulleted_list',
+        attributes: [],
+        properties: [
+          {
+            propName: 'title',
+            value: [
+              {
+                text: 'list1.2',
+                atts: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        blockId: '157cbd6b-a2ac-4dd1-b2d5-9c3e5eac6683',
+        blockIds: [],
+        type: 'bulleted_list',
+        attributes: [],
+        properties: [
+          {
+            propName: 'title',
+            value: [
+              {
+                text: 'list2',
+                atts: [],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    out: `
+    <bulleted_list.x>
+      <bulleted_list>list1
+        <bulleted_list.x>
+          <bulleted_list>list1.1</bulleted_list>
+          <bulleted_list>list1.2</bulleted_list>
+        </bulleted_list.x>
+      </bulleted_list>
+      <bulleted_list>list2</bulleted_list>
+    </bulleted_list.x>
+    `,
+  },
+  {
+    pageId: '517b209c-981e-439e-9b8f-bd04d4ca48a8',
+    in: [
+      {
+        blockId: '517b209c-981e-439e-9b8f-bd04d4ca48a8',
+        blockIds: [
+          '3c0878e1-e6c5-4e85-b757-8355262577bf',
+          '157cbd6b-a2ac-4dd1-b2d5-9c3e5eac6683',
+        ],
+        type: 'page',
+        attributes: [
+          {
+            att: 'pageIcon',
+            value: '',
+          },
+        ],
+        properties: [
+          {
+            propName: 'title',
+            value: [
+              {
+                text: 'page 2',
+                atts: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        blockId: '3c0878e1-e6c5-4e85-b757-8355262577bf',
+        blockIds: [
+          '6664db04-32e0-4996-9a30-2a5e353c5902',
+          '699e463d-cfe1-458d-b146-05b2f8ba489d',
+          '1f21514e-b820-4ac5-972a-dfdcfc4403aa',
+          '74870e62-22a3-4609-a0cc-74ac823262b3',
+        ],
+        type: 'bulleted_list',
+        attributes: [],
+        properties: [
+          {
+            propName: 'title',
+            value: [
+              {
+                text: 'list1',
+                atts: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        blockId: '6664db04-32e0-4996-9a30-2a5e353c5902',
+        blockIds: [],
+        type: 'numbered_list',
+        attributes: [],
+        properties: [
+          {
+            propName: 'title',
+            value: [
+              {
+                text: 'n1.1',
+                atts: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        blockId: '699e463d-cfe1-458d-b146-05b2f8ba489d',
+        blockIds: [],
+        type: 'numbered_list',
+        attributes: [],
+        properties: [
+          {
+            propName: 'title',
+            value: [
+              {
+                text: 'n1.2',
+                atts: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        blockId: '1f21514e-b820-4ac5-972a-dfdcfc4403aa',
+        blockIds: [],
+        type: 'bulleted_list',
+        attributes: [],
+        properties: [
+          {
+            propName: 'title',
+            value: [
+              {
+                text: 'list1.2',
+                atts: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        blockId: '74870e62-22a3-4609-a0cc-74ac823262b3',
+        blockIds: [],
+        type: 'bulleted_list',
+        attributes: [],
+        properties: [
+          {
+            propName: 'title',
+            value: [
+              {
+                text: 'list1.3',
+                atts: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        blockId: '157cbd6b-a2ac-4dd1-b2d5-9c3e5eac6683',
+        blockIds: [],
+        type: 'bulleted_list',
+        attributes: [],
+        properties: [
+          {
+            propName: 'title',
+            value: [
+              {
+                text: 'list2',
+                atts: [],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    out: `
+    <bulleted_list.x>
+      <bulleted_list>list1
+        <numbered_list.x>
+          <numbered_list>n1.1</numbered_list>
+          <numbered_list>n1.2</numbered_list>
+        </numbered_list.x>
+        <bulleted_list.x>
+          <bulleted_list>list1.2</bulleted_list>
+          <bulleted_list>list1.3</bulleted_list>
+        </bulleted_list.x>
+      </bulleted_list>
+      <bulleted_list>list2</bulleted_list>
+    </bulleted_list.x>
+    `,
   },
 ];
 
@@ -80,6 +351,8 @@ describe('renderNotionBlocks', () => {
       fixture.in,
       factory.renderFuncs(),
     );
-    expect(factory.toString(result)).toEqual(fixture.out);
+    expect(factory.toString(result)).toEqual(
+      fixture.out.replace(/[ \r\n]/g, ''),
+    );
   });
 });
