ba9437e2125aac40c68218ff7da3d5c35a35f033
Pierre Carion <pcarion@gmail.com>
2020-01-12 10:01:21 -0800

    fix casing problem

--- examples/plugin-site/src/templates/page.js
+++ examples/plugin-site/src/templates/page.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import notionRendererFactory from 'gatsby-source-notionso/lib/renderer';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import NotionBlockRenderer from '../components/notionBlockRenderer';
 
 const Template = ({ data, pageContext }) => {
