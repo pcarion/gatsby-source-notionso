4d0943bbea315b7a1cbcb94d4d608996cebc2c3a
Pierre Carion <pcarion@gmail.com>
2020-01-12 16:30:22 -0800

    feature: add netlify integration

--- examples/plugin-site/gatsby-config.js
+++ examples/plugin-site/gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
         name: 'Blog',
         rootPageUrl:
           'https://www.notion.so/gatsby-source-notionso-80562eadd8334396a905c372791cbf1b',
-        debug: true,
+        debug: false,
       },
     },
     {
