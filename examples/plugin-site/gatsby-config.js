83d1f6e672c7cdeb1153767732af4655467cba53
Pierre Carion <pcarion@gmail.com>
2020-01-14 21:52:23 -0800

    docs: remove debug mode

--- examples/plugin-site/gatsby-config.js
+++ examples/plugin-site/gatsby-config.js
@@ -2,14 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'gatsby-source-notionso example site',
     description:
-      'series of article to explain how to use the gatsby-source-notionso',
+      'series of article to explain how to use the gatsby-source-notionso plugin',
     basePath: '/',
   },
   plugins: [
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-156098793-1",
+        trackingId: 'UA-156098793-1',
         head: true,
         anonymize: true,
         respectDNT: true,
@@ -20,7 +20,7 @@ module.exports = {
       options: {
         name: 'Blog',
         rootPageUrl: process.env.GATSBY_NOTIONSO_ROOT_PAGE_URL,
-        debug: true,
+        debug: false,
       },
     },
     {
