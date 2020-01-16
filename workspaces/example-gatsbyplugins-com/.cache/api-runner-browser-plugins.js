0488355a63056ca2a69e164696ccc66de2d4fafa
Pierre Carion <pcarion@hotmail.com>
2020-01-15 20:56:27 -0800

    chore: transform project into a multi repo using yarn workspaces

---
+++ workspaces/example-gatsbyplugins-com/.cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('../../../node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[],"trackingId":"UA-156098793-1","head":true,"anonymize":true,"respectDNT":true},
+    }]
